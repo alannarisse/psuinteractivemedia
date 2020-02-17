d75e4816fa831cb65babd69b87a58f0b33d8fb77
alannarisse <angrypirate@gmail.com>
2020-02-17 13:19:06 -0800

    added p5 demo files

---
+++ in-class-demos/p5-demo/class-example/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+  print('400 by 400 canvas');
+}
+
+function draw() {
+	background(255,215,244);
+	ellipse(200, 200, 80, 80);
+	// console.log(frameCount);
+  // put drawing code here
+}
---
+++ in-class-demos/p5-demo/sketch-test.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+}
+
+function draw() {
+	background(0);
+	ellipse(200, 200, 50, 50);
+  // put drawing code here
+}
