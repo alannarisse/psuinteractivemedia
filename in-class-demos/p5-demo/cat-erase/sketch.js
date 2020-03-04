98259c0ae651bc0a99823acb7f4bf060c439da57
alannarisse <angrypirate@gmail.com>
2020-03-04 13:24:43 -0800

    added students

--- in-class-demos/p5-demo/cat-erase/sketch.js
+++ in-class-demos/p5-demo/cat-erase/sketch.js
@@ -1,21 +1,29 @@
 function setup() {
   // put setup code here
   createCanvas(640,480);
-  background(0,0,0);
+  bg = loadImage('http://placekitten.com/1200/800');
+      ellipse(mouseX,mouseY,100,100);
+
+  erase();
+
 }
 
 function draw() {
   noStroke();
+  background(bg);
+
   fill(255,187,179,80);
   ellipse(320,240,100,100);
 
   noStroke();
-  erase();
+  
   //fill(187,255,179,80);
-  ellipse(mouseX,mouseY,100,100);
   // put drawing code here
 }
 
 function mousePressed() {
-  background(151,232,213);
+  if (bg) {
+    bg.remove();
+    bg = null;
+  }
 }
