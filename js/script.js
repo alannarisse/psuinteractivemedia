0a5d52642937bc2220a696b66ca734250404d93e
alannarisse <angrypirate@gmail.com>
2020-09-27 21:55:43 -0700

    adding lfs support

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function topNavList() {
   $("#topnavlist").html(
     `<li><a href="/index.html">Home</a></li>
-        <li><a href="/portfolios.html">Homework</a></li>
+        <li><a href="/homework/index.html">Homework</a></li>
         <li><a href="/projects.html">Projects</a></li>
         <li><a href="/resources.html">Resources</a></li>
         <li><a href="/glossary.html">Glossary</a></li>
@@ -13,8 +13,8 @@ function footerNavList() {
   $("#footernavlist").html(
     `<li><a href="mailto:arisse@pdx.edu">email alanna</a></li>
     <li><a href="/terms/fa20des341/index.html">student links</a></li>
-      <li><a href="portfolios.html">Portfolio Inspiration</a></li>
-      <li><a href="keep-sane.html">Keep Sane</a></li>
+      <li><a href="/portfolios.html">Portfolio Inspiration</a></li>
+      <li><a href="/keep-sane.html">Keep Sane</a></li>
       <li><a href="https://psu.gd" target="_blank">PSU.gd</a></li>
       <li><a href="https://www.facebook.com/PSUArtandDesign/?tn-str=k*F" target="_blank">PSUGD on Facebook</a></li>
       <li><a href="https://designportland.org/directory" target="_blank">Design PDX Directory</a></li>
