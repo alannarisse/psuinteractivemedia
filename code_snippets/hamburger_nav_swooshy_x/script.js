e73fcb5fa2af1dec7e51011cb462a2ea6625f2e6
alannarisse <angrypirate@gmail.com>
2020-11-09 10:23:26 -0800

    added fairy lights

--- code_snippets/hamburger_nav_swooshy_x/script.js
+++ code_snippets/hamburger_nav_swooshy_x/script.js
@@ -1,32 +1,29 @@
-'use strict'
-
+"use strict"
 
 function toggleIcon() {
-    $('.icon').on('click', function(){
-        $('.icon').toggleClass('active');
-        $('.menu-mobile').slideToggle(300);
-    })
+  $(".icon").on("click", function () {
+    $(".icon").toggleClass("active")
+    $(".menu-mobile").slideToggle(300)
+  })
 }
 
-function closeMobileMenu(){
-    $('.menu-mobile').on('click', 'a', function(){
-        $('.icon').trigger('click');
-    })
+function closeMobileMenu() {
+  $(".menu-mobile").on("click", "a", function () {
+    $(".icon").trigger("click")
+  })
 }
-
-
 
 function desktopMenu() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
-            $('header.desktop').fadeIn(500);
-        } else {
-            $('header.desktop').fadeOut(500);
-        }
-    })
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+      $("nav.desktop").fadeIn(500)
+    } else {
+      $("nav.desktop").fadeOut(500)
+    }
+  })
 }
 
 //when the page loads call toggleIcon;
-$(toggleIcon);
-$(closeMobileMenu);
-$(desktopMenu);
+$(toggleIcon)
+$(closeMobileMenu)
+$(desktopMenu)
