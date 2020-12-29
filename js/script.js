ce589aadba6b7c3dd3ceba6f13c85033839390d7
alannarisse <angrypirate@gmail.com>
2020-12-29 11:02:44 -0800

    added new colors and portfolio links

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function topNavList() {
         <li><a href="/videos/index.html">Videos</a></li>
         <li><a href="/projects.html">Projects</a></li>
         <li><a href="/resources.html">Resources</a></li>
-        <li><a href="/glossary.html">Glossary</a></li>
+        <li><a href="/terms/wi21des34x/index.html">Student Links</a></li>
         <li><a href="mailto:arisse@pdx.edu" class="button">email alanna</a></li>`
   )
 }
@@ -16,6 +16,8 @@ function footerNavList() {
       <li><a href="/portfolios.html">Portfolio Inspiration</a></li>
       <li><a href="/keep-sane.html">Keep Sane</a></li>
       <li><a href="https://psu.gd" target="_blank">PSU.gd</a></li>
+      <li><a href="/glossary.html">Glossary</a></li>
+
       <li><a href="https://www.facebook.com/PSUArtandDesign/?tn-str=k*F" target="_blank">PSUGD on Facebook</a></li>
       <li><a href="https://designportland.org/directory" target="_blank">Design PDX Directory</a></li>
     <li>&copy; Copyright Alanna Risse 2020/2021</li>`
