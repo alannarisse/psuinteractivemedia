94a7d93d09d4163d4b7e5869284ac700a23e9dcc
alannarisse <angrypirate@gmail.com>
2021-01-25 13:23:02 -0800

    added all kinds of fun stuff for week 4

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function topNavList() {
   $("#topnavlist").html(
     `<li><a href="/index.html">Home</a></li>
         <li><a href="/videos/index.html">Videos</a></li>
-        <li>Basecamps: <a href="https://3.basecamp.com/3840950/projects/20215530">341</a> <a href="https://3.basecamp.com/3840950/projects/20215531">342</a> &nbsp;</li>
+        <li>Basecamps: <br/><a href="https://3.basecamp.com/3840950/projects/20215530">341</a> <a href="https://3.basecamp.com/3840950/projects/20215531">342</a> &nbsp;</li>
         <li><a href="/resources.html">Resources</a></li>
         <li><a href="/terms/wi21des34x/index.html">Student Links</a></li>
         <li><a href="http://teaching.alannarisse.com/grades/">Gradesheet</a></li>
