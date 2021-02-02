c8d3d466df4d1f839bc4c475f880fd3ee10be77e
alannarisse <angrypirate@gmail.com>
2021-02-01 19:55:28 -0800

    added mujtabas demo code

---
+++ code_snippets/slides-mujtaba/script.js
@@ -0,0 +1,25 @@
+// your script file for adding your own jquery
+$(function () {
+  // Your Code from here on down. Don't delete that line above!
+
+  $(".single-slide").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    pauseOnDotsHover: true,
+    arrows: false,
+    dots: true
+  })
+
+  $(".pause").on("click", function () {
+    $(".slider").slick("slickPause")
+  })
+
+  $(".play").on("click", function () {
+    $(".slider").slick("slickPlay")
+  })
+
+  // End of Your Code . Don't delete that line below!!
+})
