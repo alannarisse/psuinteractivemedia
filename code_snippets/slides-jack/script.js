e1e5dd59acec22577c97fdd636f3f1e806bbe84c
alannarisse <angrypirate@gmail.com>
2021-03-03 17:53:35 -0800

    added jack slides

---
+++ code_snippets/slides-jack/script.js
@@ -0,0 +1,76 @@
+// your script file for adding your own jquery
+$(function() {
+// Your Code from here on down. Don't delete that line above!
+
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 2
+});
+
+$('.autoplay').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3500,
+});
+
+// code for single slider carousel
+$('.single-slide').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  infinite: true,
+  dots: true,
+  draggable: false
+});
+
+
+$('.my-slick').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+      	centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+      	centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+      	centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+  // End of Your Code . Don't delete that line below!!
+
+});
