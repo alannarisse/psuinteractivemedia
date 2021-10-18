77a698d5cfeaef240ee0d746fd0f77409fe94a4a
alannarisse <angrypirate@gmail.com>
2021-10-18 10:41:51 -0700

    added fall links

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function topNavList() {
         <li><a href="/videos/index.html">Videos</a></li>
         <li><a href="https://3.basecamp.com/3840950/projects/21449773">Basecamps</a></li>
         <li><a href="/resources.html">Resources</a></li>
-        <li><a href="/terms/sp21des342/index.html">Student Links</a></li>
+        <li><a href="/terms/fa21des341/index.html">Student Links</a></li>
         <li><a href="http://teaching.alannarisse.com/grades/">Gradesheet</a></li>
         <li><a href="/inspiring.html">Inspiration</a></li>
         <li><a href="mailto:arisse@pdx.edu" class="button">email alanna</a></li>`
@@ -14,7 +14,7 @@ function topNavList() {
 function footerNavList() {
   $("#footernavlist").html(
     `<li><a href="mailto:arisse@pdx.edu">email alanna</a></li>
-    <li><a href="/terms/wi21des34x/index.html">student links</a></li>
+    <li><a href="/terms/fa21des341/index.html">student links</a></li>
       <li><a href="/portfolios.html">Portfolio Inspiration</a></li>
       <li><a href="/keep-sane.html">Keep Sane</a></li>
       <li><a href="https://psu.gd" target="_blank">PSU.gd</a></li>
