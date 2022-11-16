a6fa1a3b19546202dd0b0057f30fbd0558140457
alannarisse <angrypirate@gmail.com>
2022-11-16 08:23:55 -0800

    added layout to horizontal sroll site

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ function topNavList() {
         <li><a href="/resources.html">Resources</a></li>
         <li><a href="/terms/current/index.html">Student Links</a></li>
         <li><a href="/inspiring.html">Inspiration</a></li>
-        <li><a href="mailto:arisse@pdx.edu" class="button">email alanna</a></li>`
+        <li><a href="/inspiration-4-projects.html">Projects</a></li>
+        `
   )
 }
 
