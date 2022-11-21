d87a697b15e1d82900cbc9e40657651305b8f7e1
alannarisse <angrypirate@gmail.com>
2022-11-21 13:27:41 -0800

    added emo slides

---
+++ code_snippets/slick-emo/script.js
@@ -0,0 +1,18 @@
+// your script file for adding your own jquery
+$(function () {
+  // Your Code from here on down. Don't delete that line above!
+
+  // code for single slider carousel
+  $('.single-slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 3500,
+    infinite: true,
+    dots: true,
+    draggable: false
+  })
+
+  // End of Your Code . Don't delete that line below!!
+})
