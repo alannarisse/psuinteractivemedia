82078469793ccc44cdf4f095256fc78264acf3aa
alannarisse <angrypirate@gmail.com>
2022-11-20 18:59:04 -0800

    updated site

--- js/script.js
+++ js/script.js
@@ -13,16 +13,12 @@ function topNavList() {
 function footerNavList() {
   $('#footernavlist').html(
     `<li><a href="mailto:arisse@pdx.edu">email alanna</a></li>
-    <li><a href="/terms/current/index.html">student links</a></li>
       <li><a href="/portfolios.html">Portfolio Inspiration</a></li>
-      <li><a href="/keep-sane.html">Keep Sane</a></li>
-      <li><a href="https://psu.gd" target="_blank">PSU.gd</a></li>
       <li><a href="/glossary.html">Glossary</a></li>
 
-      <li><a href="https://www.facebook.com/PSUArtandDesign/?tn-str=k*F" target="_blank">PSUGD on Facebook</a></li>
       <li><a href="https://designportland.org/directory" target="_blank">Design PDX Directory</a></li>
       <li><a href="https://www.pdx.edu/registration/academic-calendar">PSU calendar</a></li>
-    <li>&copy; Copyright Alanna Risse 2021</li>`
+    <li>&copy; Alanna Risse 2022</li>`
   )
 }
 function init() {
