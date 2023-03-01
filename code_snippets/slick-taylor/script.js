a9024b11093e79699bfa21f9bda9cbede857a019
alannarisse <angrypirate@gmail.com>
2023-03-01 11:28:13 -0800

    added slides

---
+++ code_snippets/slick-taylor/script.js
@@ -0,0 +1,23 @@
+// your script file for adding your own jquery
+$(function() {
+// Your Code from here on down. Don't delete that line above!
+
+
+// code for single slider carousel
+$('.single-slide').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: true,
+  autoplaySpeed: 1500,
+  infinite: true,
+  dots: true,
+  draggable: false
+});
+
+
+
+
+  // End of Your Code . Don't delete that line below!!
+
+});
