6aa03047460ab222fdd420df9ec6fd7ecb58b8d1
alannarisse <angrypirate@gmail.com>
2023-03-08 10:19:19 -0800

    simple list 2

--- code_snippets/shopping-list/script.js
+++ code_snippets/shopping-list/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
+  $('.shoplist').append(`<li>${localStorage.getItem('todo')}</li>`)
+
   $('#addBtn').click(function () {
     let input = $('#newItem').val()
     console.log(input)
-    $('.shoplist').append(`<li>${input}</li>`)
+    localStorage.setItem('todo', `${input}`)
+
+    $('.shoplist').append(`<li>${localStorage.getItem('todo')}</li>`)
     $('#newItem').val('')
   })
 })
