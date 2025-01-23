492bf3b49d68ca0e7ce8574ae5741a524024ba0d
alannarisse <angrypirate@gmail.com>
2025-01-22 17:08:05 -0800

    added resources section with a flow chart chart

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ function footerNavList() {
       <li><a href="/portfolios.html">Portfolio Inspiration</a></li>
       <li><a href="/glossary.html">Glossary</a></li>
       <li><a href="https://www.pdx.edu/registration/academic-calendar">PSU calendar</a></li>
-    <li>&copy; Alanna Risse 2023</li>`
+    <li>&copy; Alanna Risse 2025</li>`
   )
 }
 function init() {
