b94e4540fd93401ad6a7ce9a873d5e19941a483e
alannarisse <angrypirate@gmail.com>
2026-03-30 12:36:01 -0700

    moved des140

---
+++ terms/everything-component.js
@@ -0,0 +1,32 @@
+function masterList() { 
+  $('#master').html(`
+  <h2>the full list of past projects</h2>
+      <ul>
+      <li><a href="/terms/wi26des140/">Winter 26 DES140</a></li>
+        <li><a href="/terms/sp25des340/">Spring 25 DES340</a></li>
+        <li><a href="/terms/sp25des342/">Spring 25 DES342</a></li>
+        <li><a href="/terms/wi25des340/">Winter 25 DES340</a></li>
+        <li><a href="/terms/fa23des341/">Fall 23 DES341</a></li>
+        <li><a href="/terms/sp23des34x/">Spring 23 DES340, DES342</a></li>
+        <li><a href="/terms/wi23des341/">Winter 23 DES341</a></li>
+        <li><a href="/terms/sp22des342/">Spring 22 DES342</a></li>
+        <li><a href="/terms/wi22des341/">Winter 22 DES341</a></li>
+        <li><a href="/terms/fa22des341">Fall 22 DES341</a></li>
+        <li><a href="/terms/sp21des342">Spring 21 DES342</a></li>
+        <li><a href="/terms/wi21des34x/">Winter 21 DES341, DES342</a></li>
+        <li><a href="/terms/fa21des341">Fall 21 DES341</a></li>
+        <li><a href="/terms/sp20des342">Spring 20 DES342</a></li>
+        <li><a href="/terms/wi20des342/">Winter 20 DES342</a></li>
+        <li><a href="/terms/wi20des341/">Winter 20 DES341</a></li>
+        <li><a href="/terms/fa20des341">Fall 20 DES341</a></li>
+        <li><a href="/terms/fa19des341">Fall 19 DES341</a></li>
+
+        <li>-----</li>
+        <li><a href="/terms/past/">The class archives</a></li>
+
+      </ul>
+  `)
+}
+
+
+$(masterList())
